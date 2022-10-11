ad29dd481f83495cfdaa19ea534ff3a4f8e69072
prathmeshg26 <113121523+prathmeshg26@users.noreply.github.com>
2022-10-11 13:06:56 +0530

    Add files via upload

---
+++ 11 oct/multiplication.java
@@ -0,0 +1,25 @@
+/* QesPrint any multiplication table using for loop
+ * 
+ * 
+ * 		NAME:-PRATHMESH RAJESH GONDHALI
+ * 		DATE:-11/10/2022
+ */
+package javacore;
+
+public class multiplication {
+
+	public static void main(String[] args) {
+		
+java.util.Scanner sc = new java.util.Scanner(System.in);
+		
+		System.out.print("Enter the number : ");
+		int x = sc.nextInt();
+			
+		for(int i=1;i<=10;i++)	{
+		System.out.println(x*i);
+			
+		}
+		
+	}
+
+}
