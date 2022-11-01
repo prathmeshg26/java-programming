903acda53a41d0cd487af807340b00e662d3f646
prathmeshg26 <113121523+prathmeshg26@users.noreply.github.com>
2022-11-01 21:38:49 +0530

    Add files via upload

---
+++ Lab section/LAB 27OCT/Happy_Diwali.java
@@ -0,0 +1,26 @@
+/*Q1.WAP to print HAPPY DIWALI using any loop.
+ * NAME:PRATHMESH RAJESH GONDHALI
+ * DATE:27-10-2022
+ */
+package javaversion;
+
+public class Happy_Diwali {
+	//Using Main Method
+	public static void main(String[] args) {
+		
+		//declaring the string 
+				String s= "HAPPY DIWALI";
+
+				char[] arr=s.toCharArray();
+		        //Using for loop
+				for (int i=0;i<s.length();i++)
+				{
+		        //USING System.out.println(string);
+				System.out.println(s);
+				//break statement to exit the loop.
+				break;
+				}
+
+	}
+
+}
